d19994d70e536ab7b884b5d32d7e479cbe09bc94
19013886 <19013886@myrp.edu.sg>
2021-07-06 10:59:56 +0800

    P08 - Checkpoint 2 "Save and retrieve the different user input data" done

---
+++ app/src/main/java/sg/edu/rp/c346/id19013886/demomyprofile/MainActivity.java
@@ -0,0 +1,63 @@
+package sg.edu.rp.c346.id19013886.demomyprofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etName, etGPA;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.editTextName);
+        etGPA = findViewById(R.id.editTextGPA);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //Step 1a: Get the user input from the EditText and store it in a variable
+        String strName = etName.getText().toString();
+        Float gpa = Float.parseFloat(etGPA.getText().toString());
+
+        //Step 1b: Obtain an instance of the SharedPreferences
+        SharedPreferences pref = getPreferences(MODE_PRIVATE);
+
+        //Step 1c: Obtain an instance of the SharedPreferences Editor for update later
+        SharedPreferences.Editor prefEdit = pref.edit();
+
+        //Step 1d: Add the key-value pair
+        prefEdit.putString("name", strName);
+        prefEdit.putFloat("gpa", gpa);
+
+        //Step 1e: Call commit() to save the changes into SharedPreferences
+        prefEdit.commit();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //Step 2a: Obtain an instance of the SharedPreferences
+        SharedPreferences pref = getPreferences(MODE_PRIVATE);
+
+        //Step 2b: Retrieve the saved data from the SharedPreferences object
+        String strName = pref.getString("name", "John");
+        Float gpa = pref.getFloat("gpa", 3);
+
+        //Step 2c: Update the UI element with the value
+        etName.setText(strName);
+        etGPA.setText(gpa + "");
+
+    }
+
+}
